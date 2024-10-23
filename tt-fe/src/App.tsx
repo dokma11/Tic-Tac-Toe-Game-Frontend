ee3a03321789de6d9456edc5d2d82b644c013e2d
dokma11 <vule.dok@gmail.com>
2024-10-23 11:46:57 +0200

    style: add a 404 not found page

--- tt-fe/src/App.tsx
+++ tt-fe/src/App.tsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
 import SignUpPage from "./pages/SignUpPage.tsx";
 import PlayPage from "./pages/PlayPage.tsx";
+import NotFoundPage from "./pages/NotFoundPage.tsx";
 
 function App() {
     const router = createBrowserRouter(
@@ -13,6 +14,7 @@ function App() {
                 <Route path='/login' element={<LoginPage />} />
                 <Route path='/register' element={<SignUpPage />} />
                 <Route path='/play' element={<PlayPage />} />
+                <Route path='*' element={<NotFoundPage />} />
             </Route>
         )
     );
